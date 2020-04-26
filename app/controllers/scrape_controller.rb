72dd463975673e45f8bf5c95a3a356e37df6cc63
makita-piano <makita@piano.or.jp>
2020-04-26 16:26:27 +0900

    json出力できるように

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -1,4 +1,15 @@
 class ScrapeController < ApplicationController
   def index
+    @json_h = {}
+    @json_h["a"]=1
+    @json_h["b"]=1
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @json_h }
+    end
   end
+
+private
+
 end
